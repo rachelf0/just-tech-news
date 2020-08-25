7e55b305b921230b09f74c706064701f998dd308
Rachel Fritz <rachelfritz0@gmail.com>
2020-08-25 09:34:21 -0500

    push to develop

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: false }).then(() => { // sequelize taking the models and connecting them to associated database tables
   app.listen(PORT, () => console.log('Now listening'));
 });
+// if force was set to true it would drop and re-create all the database tables on startup
+// force true will make tables re-create in there are any association changes
